6430fadee8af688579954b0489f0b4092e2dd8b2
BelkevichAndrew <belkevichandry@gmail.com>
2018-10-28 16:22:19 +0300

    jenkins test commit

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-import get from './services/redis-service'
+// import get from './services/redis-service'
+import * as cors from "cors";
+
 
 app.get('/:search', (req, res) => {
-    get(req.params.search, data => {
-        res.send(data)
-    });
+    // get(req.params.search, data => {
+    //     res.setHeader('Access-Control-Allow-Origin', '*');
+    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //     res.setHeader('Access-Control-Allow-Credentials', true);
+    //     res.send(data)
+    // });
+    res.send("test")
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
